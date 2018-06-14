0e6169c22e89c27957884e4b45b219a444b17b08
ee <ee@cnclub.asia>
2018-06-14 13:45:35 +0800

    fix(config): wrong key name
    Key => key
    
    BREAKING CHANGE: config key name changed
    
        Before:
    
        'Key' => env('STEAM_APIKEY')
        config('steam-api.Key')
    
        After:
    
        'key' => env('STEAM_APIKEY')
        config('steam-api.key')

--- src/config/config.php
+++ src/config/config.php
@@ -6,5 +6,5 @@ return array(
 	 * You can get a steam API key from https://steamcommunity.com/dev/apikey
 	 * Once you get your key, add it here.
 	*/
-	'Key' => env('STEAM_APIKEY'),
+	'key' => env('STEAM_APIKEY'),
 );
